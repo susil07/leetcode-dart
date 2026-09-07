cffcd5091deb73e06e7c322e1474170061d7f527
Susil Kumar Dora <susilkumardora456@gmail.com>
2026-09-07 22:26:26 +0530

    Solve #169 Majority Element

---
+++ easy/0169_majority_element/solution.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int majorityElement(List<int> nums) {
+    int candidate = 0;
+    int count = 0;
+
+    for (int num in nums) {
+      if (count == 0) {
+        candidate = num;
+      }
+
+      if (num == candidate) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+
+    return candidate;
+  }
+}
